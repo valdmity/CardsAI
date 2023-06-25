61e895e68eaedc2e69170a7d679f84e62c15abd2
Denis <dlsayafarov@gmail.com>
2023-06-25 20:27:07 +0500

    добавил адаптивный интерфейс + visual fixes

--- src/game.js
+++ src/game.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const HOST = 'http://185.87.50.169:8000'
+const HOST = 'http://127.0.0.1:8000'; //'http://185.87.50.169:8000'
 const board = document.querySelector('.board');
 const cardsDiv = document.getElementById('cards');
 const left = document.querySelector('.left-dot .dot-text');
